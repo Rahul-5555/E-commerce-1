e22b6497f9ca0b730c4cf5d9a76da8aadbd98165
Rahul-5555 <rahul.malviya676@gmail.com>
2024-06-27 12:21:04 +0530

    deploy

--- backend/controller/user/userLogout.js
+++ backend/controller/user/userLogout.js
@@ -1,20 +1,25 @@
-async function userLogout(req,res){
-  try{
-      res.clearCookie("token")
+async function userLogout(req, res) {
+    try {
+        const tokenOption = {
+            httpOnly: true,
+            secure: true,
+            sameSite: 'none' // to allow cookie to be sent to frontend domain(different domain)
+        }
+        res.clearCookie("token",tokenOption)
 
-      res.json({
-          message : "Logged out successfully",
-          error : false,
-          success : true,
-          data : []
-      })
-  }catch(err){
-      res.json({
-          message : err.message || err  ,
-          error : true,
-          success : false,
-      })
-  }
+        res.json({
+            message: "Logged out successfully",
+            error: false,
+            success: true,
+            data: []
+        })
+    } catch (err) {
+        res.json({
+            message: err.message || err,
+            error: true,
+            success: false,
+        })
+    }
 }
 
 
